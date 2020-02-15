4fbb8006b407436529886f73e6348350f15cf8b8
mukhindev <mukhin.dev@gmail.com>
2020-02-15 20:37:17 +0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,51 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar app dark elevate-on-scroll color="blue">
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Colander</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          active-class="blue--text"
+        >
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Главная</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item to="/editor">
+            <v-list-item-icon>
+              <v-icon>mdi-axis-arrow</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Редактор</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld
-  },
-
   data: () => ({
-    //
+    drawer: false
   })
 }
 </script>
---
+++ src/views/Editor.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>Редактор</h1>
+    <svg>
+      <circle fill="black" r="4" :cx="cx" cy="100" />
+    </svg>
+    <v-input type="number" >Default Slot</v-input>
+    <v-text-field label="Ширина" v-model="cx" :rules="rules" />
+  </div>
+</template>
+
+<style scoped>
+svg {
+  width: 350px;
+  height: 200px;
+  background-color: bisque;
+}
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      cx: 200,
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters'
+      ]
+    }
+  }
+}
+</script>
