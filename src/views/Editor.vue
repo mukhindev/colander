c22d5fabec907d7287010288bbc1b0f9c515fa57
mukhindev <mukhin.dev@gmail.com>
2020-02-16 00:00:55 +0300

    editor init

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="about">
-    <h1>Редактор</h1>
-    <svg>
-      <circle fill="black" r="4" :cx="cx" cy="100" />
-    </svg>
-    <v-input type="number" >Default Slot</v-input>
-    <v-text-field label="Ширина" v-model="cx" :rules="rules" />
+  <div class="editor">
+    <div class="preview">
+      <svg :width="w * zoom" :height="h * zoom">
+        <circle fill="black" :r="r * zoom" :cx="cx * zoom" :cy="cy * zoom" />
+      </svg>
+    </div>
+    <div class="zoom">
+      <v-btn text icon color="secondary" @click="zoomMinus">
+        <v-icon>mdi-magnify-minus</v-icon>
+      </v-btn>
+      <v-btn text icon color="secondary" @click="zoomPlus">
+        <v-icon>mdi-magnify-plus</v-icon>
+      </v-btn>
+    </div>
+    <v-text-field label="h" v-model="h" suffix="мм" />
+    <v-text-field label="w" v-model="w" suffix="мм" />
+    <v-text-field label="x" v-model="cx" suffix="мм" :rules="rules" />
+    <v-text-field label="y" v-model="cy" suffix="мм" :rules="rules" />
+    <v-text-field label="Ширина" v-model="cx" suffix="мм" :rules="rules" />
   </div>
 </template>
 
 <style scoped>
 svg {
-  width: 350px;
-  height: 200px;
   background-color: bisque;
+  transition: .25s;
+  margin: auto;
+}
+.preview {
+  overflow: scroll;
+  height: 50vh;
+  background-color: blue;
 }
 </style>
 
@@ -21,11 +38,25 @@ svg {
 export default {
   data () {
     return {
-      cx: 200,
+      zoom: 1,
+      h: 356,
+      w: 596,
+      cx: 32,
+      cy: 32,
+      r: 4,
       rules: [
         value => !!value || 'Required.',
-        value => (value && value.length >= 3) || 'Min 3 characters'
+        value => (value >= 0) || 'Выходит за пределы',
+        value => (value <= 350) || 'Выходит за пределы'
       ]
+    }
+  },
+  methods: {
+    zoomMinus () {
+      if (this.zoom > 0.2) this.zoom = this.zoom - 0.1
+    },
+    zoomPlus () {
+      if (this.zoom <= 2) this.zoom = this.zoom + 0.1
     }
   }
 }
