bdd2ecec0347ac8371a52fd79342699938f7f263
mukhindev <mukhin.dev@gmail.com>
2020-10-31 17:44:14 +0300

    Updates

---
+++ precache-manifest.5f0296136382bba2703c7349f180bec0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81530230cea010a0c1d4",
+    "url": "/colander/css/app.7203e516.css"
+  },
+  {
+    "revision": "781f1a244f2064031a6b",
+    "url": "/colander/css/chunk-vendors.0c525347.css"
+  },
+  {
+    "revision": "89f57b1b9ddf0ccded0f",
+    "url": "/colander/css/converter.ca6df252.css"
+  },
+  {
+    "revision": "d86a19e41bfb12c54bd3",
+    "url": "/colander/css/converter~editor.6367695a.css"
+  },
+  {
+    "revision": "bc16fb05f7a2133dccc5",
+    "url": "/colander/css/editor.73b72290.css"
+  },
+  {
+    "revision": "9c2c2005d858d90d1b92e96fa49d287f",
+    "url": "/colander/index.html"
+  },
+  {
+    "revision": "81530230cea010a0c1d4",
+    "url": "/colander/js/app.943bdf5e.js"
+  },
+  {
+    "revision": "781f1a244f2064031a6b",
+    "url": "/colander/js/chunk-vendors.2e260569.js"
+  },
+  {
+    "revision": "89f57b1b9ddf0ccded0f",
+    "url": "/colander/js/converter.8dcf269e.js"
+  },
+  {
+    "revision": "d86a19e41bfb12c54bd3",
+    "url": "/colander/js/converter~editor.b4dd936a.js"
+  },
+  {
+    "revision": "bc16fb05f7a2133dccc5",
+    "url": "/colander/js/editor.9bbafc1c.js"
+  },
+  {
+    "revision": "85a69fb196fbc9464ad6d08b83a6a70d",
+    "url": "/colander/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/colander/robots.txt"
+  }
+]);
